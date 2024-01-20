8f073f0fd93f672fd5565e631612b4a4f0a209c3
Jeff Pitcher <jpitcher3@gmail.com>
2024-01-20 16:54:14 -0500

    Alpha version, includes last 5 lap times, last lap, and difference

--- laptimer.py
+++ laptimer.py
@@ -1,40 +1,62 @@
 from tkinter import *
+from tkinter import ttk
 import time
 import RPi.GPIO as GPIO
 
 #intialize globals
 global lapstart
+global prevlap
 lapstart = time.time()
+prevlap = lapstart-lapstart
 
 ws = Tk()
-ws.geometry('600x450+1000+300')
+ws.attributes('-zoomed', True)
 ws.title("John's Lap Timer")
-ws.config(bg='#FFFFFF')
-#ws.iconbitmap('stopwatch.ico')
-ws.resizable(0,0)
+ws.config(bg='black')
 
-laptimes=()
-laptime_listbox = Listbox(ws)
-laptime_listbox.pack()
+#Determine ideal font size based on screen size:
+global font_size
+
+width = ws.winfo_screenwidth()
+height = ws.winfo_screenheight()
+
+a = int(height / 2)
+buff = len('00:00.00')
+b = int(width / (buff if buff >= 7 else 7))
+font_size = min(a, b)
+
+#Setup Frames
+f_current = Frame(ws, bg="black") #top half - current lap
+f_buttons = Frame(ws,bg="darkgray") #button frame
+f_results = Frame(ws, bg="black") #bottom half - laptimes
+f_results_left = Frame(f_results, bg="black") #bottom half - laptimes
+f_results_right = Frame(f_results, bg="black") #bottom half - laptimes
+
+laptime_listbox = Listbox(f_results_right
+                          , bg="black"
+                          , fg="white"
+                          , width=15
+                          , font=("Verdana",int(font_size/4),"bold"))
+laptime_listbox.insert(0,"Previous Laps:")
+difftime_listbox = Listbox(f_results_right
+                          , bg="grey"
+                          , fg="white"
+                          , width=10
+                          , font=("Verdana",int(font_size/4),"bold"))
+
+lbl_lastLap = Label(f_results_left,text="Last Lap:",fg="white",bg="black",font=("Veranda",int(font_size/4),"bold"),anchor="w",justify="left")
+lbl_lastLaptime = Label(f_results_left,text="00:00.000",fg="white",bg="black",font=("Veranda",int(font_size/2),"bold"),anchor="w",justify="left")
+lbl_lastLapdiff = Label(f_results_left,text="+ 00:00.000",fg="white",bg="black",font=("Veranda",int(font_size/2),"bold"),anchor="w",justify="right")
 
 counter = -1
 running = False
 def counter_label(lbl):
     def count():
         if running:
-            global counter
-
-
-            #now = datetime.now()
             global lapstart
             duration = time.time() - lapstart
-            
-
-            #lbl['text']=str(timedelta(now-lapstart))   
             lbl['text'] = time.strftime("%M:%S.{}".format(str(duration % 1)[2:])[:8], time.gmtime(duration))
-
-            lbl.after(10, count)    
-            counter += 1
+            lbl.after(60, count)    
     count()     
 
 def StartTimer(lbl):
@@ -55,54 +77,58 @@ def StopTimer():
     running = False
 
 def ResetTimer(lbl):
-    global counter
-    counter=-1
     if running==False:      
         reset_btn['state']='disabled'
-        lbl['text']='00:00.000'
+        lbl['text']='00:00.00'
     else:                          
         lbl['text']=''
 
 def MarkLapCallback(channel):
     global running
+    if running==False:
+        StartTimer(lbl)
+        return
     global lapstart 
     global lastlap
+    global prevlap
     lastlap = time.time() - lapstart
     lapstart = time.time()
-    laptime_listbox.insert(0,time.strftime("%M:%S.{}".format(str(lastlap % 1)[2:])[:8], time.gmtime(lastlap)))
-    running=True
+    
+    laptime_listbox.insert(1,time.strftime("%M:%S.{}".format(str(lastlap % 1)[2:])[:9], time.gmtime(lastlap)))
+    diff=lastlap-prevlap
+    diffstr=time.strftime("%M:%S.{}".format(str(abs(diff) % 1)[2:])[:9], time.gmtime(abs(diff)))
+    if diff<0:
+        diffstr = ["-",diffstr]
+        lbl_lastLaptime['fg']="green"
+        lbl_lastLapdiff['fg']="green"
+    else:
+        diffstr = ["+",diffstr]
+        lbl_lastLaptime['fg']="red"
+        lbl_lastLapdiff['fg']="red"
+    difftime_listbox.insert(0, diffstr)
+    lbl_lastLaptime['text']=time.strftime("%M:%S.{}".format(str(lastlap % 1)[2:])[:9], time.gmtime(lastlap))
+    lbl_lastLapdiff['text']=diffstr
+    prevlap = lastlap
 
-#bg = PhotoImage(file='stopwatch.png')
-#img = Label(ws, image=bg, bg='#299617')
-#img.place(x=75, y=50)
 
+lbl_curlab = Label(f_current,text="Current Lap:",fg="white",bg="black",font=("Veranda",int(font_size/4),"bold"),anchor="w",justify="left")
 lbl = Label(
-    ws, 
+    f_current, 
     text="00:00.00", 
-    fg="black", 
-    bg='white', 
-    font="Verdana 40 bold"
+    fg="white", 
+    bg='black', 
+    font=("Verdana",font_size,"bold")
     )
-
-label_msg = Label(
-    ws, text="minutes : seconds . milliseconds", 
-    fg="black", 
-    bg='white', 
-    font="Verdana 10 bold"
-    )
-
-lbl.place(x=160, y=170)
-label_msg.place(x=170, y=250)
 
 start_btn=Button(
-    ws, 
+    f_buttons, 
     text='Start', 
     width=15, 
     command=lambda:StartTimer(lbl)
     )
 
 stop_btn = Button(
-    ws, 
+    f_buttons, 
     text='Stop', 
     width=15, 
     state='disabled', 
@@ -110,7 +136,7 @@ stop_btn = Button(
     )
 
 reset_btn = Button(
-    ws, 
+    f_buttons, 
     text='Reset', 
     width=15, 
     state='disabled', 
@@ -118,16 +144,37 @@ reset_btn = Button(
     )
 
 lap_btn = Button(
-    ws, 
+    f_buttons, 
     text='Lap', 
     width=15, 
     command=lambda:MarkLapCallback(0)
     )
 
-start_btn.place(x=30, y=390)
-stop_btn.place(x=150, y=390)
-reset_btn.place(x=270, y=390)
-lap_btn.place(x=390, y=390)
+
+
+#do layout
+f_current.pack(fill=X)
+f_buttons.pack(fill=X)
+f_results.pack(fill=BOTH, expand=True)
+f_results_left.pack(side=LEFT,fill=X)
+f_results_right.pack(side=LEFT,fill=X)
+
+lbl_curlab.pack(fill=X)
+lbl.pack(fill=X)
+#buttons
+start_btn.pack(side=LEFT)
+stop_btn.pack(side=LEFT)
+reset_btn.pack(side=LEFT)
+lap_btn.pack(side=LEFT)
+
+#Results
+lbl_lastLap.pack(fill=X,side=TOP)
+lbl_lastLaptime.pack(fill=X,side=TOP)
+lbl_lastLapdiff.pack(fill=X,side=TOP)
+laptime_listbox.pack(fill=BOTH)
+#difftime_listbox.pack(side=LEFT,fill=BOTH)
+
+
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP) #pin 18 = GPIO 24
